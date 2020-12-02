d1813b3b2bc8ae1bf45a76af736c3bbc56d29ad0
Silviana Amethyst <silvianaamethyst@gmail.com>
2020-12-02 16:16:48 -0600

    solved day 2

---
+++ 2/day2.py
@@ -0,0 +1,42 @@
+
+
+def read_data():
+	with open ('input.txt') as f:
+		data = f.readlines()
+	return data
+
+def part1():
+	data = read_data()
+
+	count = 0
+
+	for d in data:
+		span,letter,password = d.split()
+		lower,upper = span.split('-')
+		letter = letter[0]
+		occ = password.count(letter)
+
+		if int(lower) <= occ and occ <= int(upper):
+			count = count+1
+
+	print(count)
+
+
+def part2():
+	data = read_data()
+
+	count = 0
+
+	for d in data:
+		span,letter,password = d.split()
+		pos1,pos2 = span.split('-')
+		letter = letter[0]
+
+		if (password[int(pos1)-1]==letter) != (password[int(pos2)-1] == letter):
+			count = count+1
+		else:
+			print(d)
+
+	print(count)
+
+part2()
