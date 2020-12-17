4fe0d27c1909d80e05ab665652dd57a774fe7321
Silviana Amethyst <silvianaamethyst@gmail.com>
2020-12-17 13:33:19 -0600

    solved 17.2
    almost no code had to change!!!  yay!!!  this was a very easy part2.  just attach a singleton dimension to the front of the state array in dimension 3, and a full dimension to the front in dimension 4.  it was pleasant that we didn't have to change the rules for when a cube activates.
    
    the hardest part of this problem remains understanding the given example for 3d.

--- 17/solution.py
+++ 17/solution.py
@@ -15,40 +15,44 @@ import numpy as np
 
 
 
-def initialize_state(num_cycles):
+def initialize_state(num_cycles, dimension):
 	data = read_data()
 
 	num_x = len(data[0]) + 2*num_cycles
 	num_y = len(data) + 2*num_cycles
 	num_z = 1+2*num_cycles
+	num_w = 1 if dimension==3 else 1+2*num_cycles
 
-	state_curr = np.zeros( (num_z, num_x, num_y) )
+	state_curr = np.zeros( (num_w, num_z, num_x, num_y) )
+
+	w_ind = 0 if dimension==3 else num_cycles
 
 	for ii in range(len(data)):
 		d = data[ii]
 		for jj in range(len(d)):
 			q = d[jj]
 
-			state_curr[num_cycles,num_cycles+ii,num_cycles+jj] = 1 if q=='#' else 0
+			state_curr[w_ind, num_cycles,num_cycles+ii,num_cycles+jj] = 1 if q=='#' else 0
 
 	return state_curr
 
 
-def num_active_neighbors(x,y,z, state):
-	nx, ny, nz = state.shape
+def num_active_neighbors(w,x,y,z, state):
+	nw, nx, ny, nz = state.shape
 
+	q = max(0,w-1),min(nw+1,w+2)
 	a = max(0,x-1),min(nx+1,x+2)
 	b = max(0,y-1),min(ny+1,y+2)
 	c = max(0,z-1),min(nz+1,z+2)
 
-	core = state[a[0]:a[1], b[0]:b[1], c[0]:c[1]]
+	core = state[q[0]:q[1],a[0]:a[1], b[0]:b[1], c[0]:c[1]]
 
-	num_core = threesome(state[a[0]:a[1], b[0]:b[1], c[0]:c[1]]) - state[x,y,z]
+	num_core = foursome(core) - state[w,x,y,z]
 	# only subtract 1 if the location is ON.
 	return num_core
 
-def threesome(arr):
-	return sum(sum(sum(arr)))
+def foursome(arr):
+	return sum(sum(sum(sum(arr))))
 
 
 def simulate_step(state_curr):
@@ -57,34 +61,30 @@ def simulate_step(state_curr):
 	size = state_curr.shape
 
 	for ii in range(size[0]):
-		# print(f'layer {ii}')
 		for jj in range(size[1]):
 			for kk in range(size[2]):
-
-				n = num_active_neighbors(ii,jj,kk, state_curr)
-				if state_curr[ii,jj,kk]:
-					# print(f'[{ii},{jj},{kk}] active, {n} active neighbors')
-					state_next[ii,jj,kk] = 1 if (n==2 or n==3) else 0
-				else:
-					# print(f'[{ii},{jj},{kk}] inactive, {n} active neighbors')
-					state_next[ii,jj,kk] = 1 if n==3 else 0
+				for ll in range(size[3]):
+					n = num_active_neighbors(ii,jj,kk,ll, state_curr)
+					state_curr[ii,jj,kk,ll]
+					if state_curr[ii,jj,kk,ll]:
+						state_next[ii,jj,kk,ll] = 1 if (n==2 or n==3) else 0
+					else:
+						state_next[ii,jj,kk,ll] = 1 if n==3 else 0
 
 	return state_next
 
 
 def part1():
 	num_cycles = 6
-	state_curr = initialize_state(num_cycles)
+	state_curr = initialize_state(num_cycles, 3)
 	
 
 	for c in range(num_cycles):
-		# print(f'\ncycle {c}')
 		state_next = simulate_step(state_curr)
 		state_curr = state_next
-		# print(state_curr)
 
 
-	return threesome(state_curr)
+	return foursome(state_curr)
 
 
 ###
@@ -92,12 +92,16 @@ def part1():
 
 
 def part2():
-	data = read_data()
-	thing = []
-	for a in thing:
-		pass
+	num_cycles = 6
+	state_curr = initialize_state(num_cycles, 4)
+	
 
-	return
+	for c in range(num_cycles):
+		state_next = simulate_step(state_curr)
+		state_curr = state_next
+
+
+	return foursome(state_curr)
 
 print("part 1: {}".format(part1()))
 print("part 2: {}".format(part2()))
