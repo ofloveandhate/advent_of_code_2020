3fe9c6f391a13060e881cd7587dfea6fa5870881
Silviana Amethyst <silvianaamethyst@gmail.com>
2020-12-16 22:34:40 -0600

    solved 14.2
    again, this method is totally overkill, and also totally noob.  there's probably a muuuuch better way to do this.  i'm just aiming to solve the problems, not solve them optimally.
    
    i found this a fun problem to solve, and had fun doing it using OOP.

--- 14/solution.py
+++ 14/solution.py
@@ -16,20 +16,19 @@ class Instruction(object):
 		super(Instruction, self).__init__()
 		a,b = text.split(' = ')
 
-		print(a)
 		self.loc = int(a.strip('mem[').strip(']'))
 		self.val = int(b)
 
 
 	def __str__(self):
-		return f'[{self.loc}] = val'
+		return f'[{self.loc}] = {self.val}'
 	def __repr__(self):
 		return str(self)
 		
 
 
 # destructively produces a mask and instructions
-def process_next_program(code):
+def init_next_program(code):
 	mask = code.pop(0)
 	instructions = []
 	while len(code)>0 and not code[0].startswith('mask'):
@@ -37,7 +36,7 @@ def process_next_program(code):
 
 	return mask, instructions
 
-def result(mask, val):
+def val_result(mask, val):
 	as_bin = '{0:036b}'.format(val)
 	r = ''
 	for m,b in zip(mask, as_bin):
@@ -48,9 +47,6 @@ def result(mask, val):
 		else:
 			r = r+b
 
-	print(f'val as bin: {as_bin}')
-	print(f'mask      : {mask}')
-	print(f'r         : {r}\n')
 	return int(r,2)
 
 class Program(object):
@@ -58,7 +54,7 @@ class Program(object):
 	def __init__(self, code):
 		super(Program, self).__init__()
 
-		mask, instructions = process_next_program(code)
+		mask, instructions = init_next_program(code)
 
 		self.mask = mask.strip('mask = ')
 		self.instructions = instructions
@@ -68,7 +64,42 @@ class Program(object):
 
 	def execute(self, memory):
 		for I in self.instructions:
-			memory[I.loc] = result(self.mask, I.val)
+			memory[I.loc] = val_result(self.mask, I.val)
+
+	def execute2(self,memory):
+		print(f'\nmask: {self.mask}')
+		for I in self.instructions:
+			print(f'executing instruction {I}')
+
+			loc = '{0:036b}'.format(I.loc)
+
+
+
+			for ii in range(2**self.mask.count('X'),-1,-1):
+				print(f'iteration {ii}')
+				fmt = '{:0'+str(self.mask.count('X'))+'b}'
+				as_bin = fmt.format(ii)
+				dest = ''
+				
+				for m,ell in zip(self.mask, loc):
+
+					if m=='X':
+						dest = dest+as_bin[0]
+						if len(as_bin):
+							as_bin = as_bin[1:]
+					elif m=='0':
+						dest = dest+ell
+					elif m=='1':
+						dest = dest+'1'
+					else:
+						raise ValueError()
+						
+
+				print(f'dest:  {dest}')
+				
+				dest = int(dest,2)
+				print(f'[{dest}] = {I.val}')
+				memory[dest] = I.val
 
 class Computer(object):
 	"""docstring for Computer"""
@@ -84,6 +115,10 @@ class Computer(object):
 	def execute(self):
 		for p in self.programs:
 			p.execute(self.memory)
+
+	def execute2(self):
+		for p in self.programs:
+			p.execute2(self.memory)
 
 
 	def __str__(self):
@@ -110,11 +145,10 @@ def part1():
 
 def part2():
 	data = read_data()
-	thing = []
-	for a in thing:
-		pass
 
-	return
+	c = Computer(data)
+	c.execute2()
+	return c.report()
 
 print("part 1: {}".format(part1()))
 print("part 2: {}".format(part2()))
