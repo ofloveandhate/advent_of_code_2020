042e503055b7532620387f47a7ae06e32e6f90e0
Silviana Amethyst <silvianaamethyst@gmail.com>
2020-12-17 20:55:15 -0600

    removed prints and dead code

--- 15/solution.py
+++ 15/solution.py
@@ -13,41 +13,28 @@ def write_data(data):
 def part1():
 	data = read_data()[0]
 	data = eval(f'[{data}]')
-	print(data)
 
-	# import collections
-	# times_spoken = collections.Counter()
 
 	prev_time_spoken = {}
 	for ii in range(len(data)-1):
 		prev_time_spoken[data[ii]] = ii+1
 
-	# for ii in range(len(data)-1):
-	# 	times_spoken[data[ii]] = 1
-
-	# print(times_spoken)
-	# print(prev_time_spoken)
 	seq = data[:-1]
 
 	curr_num = data[-1]
 	num_its = 30000000
 	for ii in range(len(data),num_its+1):
 
-		# print(f'\nturn {ii}')
-		# print(f'previous times: {prev_time_spoken}')
+
 		if curr_num in prev_time_spoken:
-			# print(f'{curr_num} already spoken {ii} {prev_time_spoken[curr_num]}')
 			next_num = ii-prev_time_spoken[curr_num]
 		else:
-			# print(f'{curr_num} never spoken')
 			next_num = 0
 
 		prev_time_spoken[curr_num] = ii
 		seq.append(curr_num)
 		curr_num = next_num
 
-	# print(seq)
-	# print(next_num)
 	return seq[-1]
 ###
 
